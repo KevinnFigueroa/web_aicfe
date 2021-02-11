c6792882dfcae4b6a9f1fcccc2e88f1831e01ef5
KevinnFigueroa <kevinfigueroadavid2000@gmail.com>
2021-02-11 03:10:58 -0300

    feat: agregando funcion al boton y movimiento al mapa al encontrar dirección

--- static/main.js
+++ static/main.js
@@ -1,6 +1,12 @@
-console.log("asdfa");
-
 var mymap = L.map("mapid").setView([-34.6083, -58.3712], 13);
+// API KEY: at_7NtOVTbokqDttdukaooZLHPA4focy
+
+//TODO: hacer validacion de que ese string sea una ip o un dominio.
+function takeValue(){
+    var value = document.getElementById("locationByIpAddress").value;
+    requestLocation(value);
+}
+document.getElementById("search").addEventListener("click", takeValue); 
 
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
@@ -12,3 +18,21 @@ L.control.scale().addTo(mymap);
 L.marker([41.66, -4.71], {
     draggable: true
 }).addTo(mymap);
+
+function goToNewLatLng(lat, lng){
+    mymap.panTo({lat: lat, lng: lng});
+}
+
+function requestLocation(ipAddress){
+    fetch("https://geo.ipify.org/api/v1?apiKey=at_7NtOVTbokqDttdukaooZLHPA4focy&ipAddress=" + ipAddress).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        let lat = data["location"]["lat"];
+        let lng = data["location"]["lng"];
+
+        goToNewLatLng(lat,lng);
+    }).catch(function(error){
+        console.log("error :"+ error);
+    });
+}
+
